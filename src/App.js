e6056e6817942b5ed365ec6d211c16b97bac2e55
Gaurav Dubey <gauravd638@gmail.com>
2020-12-16 20:47:07 +0530

    input with state excuted

--- src/App.js
+++ src/App.js
@@ -5,23 +5,33 @@ import React, {useState} from 'react';
 function App() {
 
   const [input,setInput] = useState('');
-  const [messages,setMessages] = useState([]);
+  const [messages,setMessages] = useState(['heloo','hi','dfndjdf']);
   console.log(input);
+  console.log(messages);
 
   const sendMessage = (event) =>{
     // all the logic to send message goes
+    event.preventDefault(); //prevent refreshing
     setMessages([...messages,input]);
+    setInput('');
+
   }
   return (
     <div className="App">
-     <h1>Hello world!</h1>
+     <h1>Messanger!</h1>
 
+    <form>
      <input value={input} onChange={event =>setInput(event.target.value)} />
-     <button onClick={sendMessage}>Send Message</button>
+     <button type="submit" onClick={sendMessage}>Send Message</button>
 
-     {/* {input feild} */}
-     {/* {button} */}
+     </form>
+     
      {/* {message themselves} */}
+     {
+       messages.map(message =>(
+         <p>{message}</p>
+  ))
+     }
     </div>
   );
 }
